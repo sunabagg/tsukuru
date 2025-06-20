a7e211627e557bac1e7721a80fabe73369db5639
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 15:34:25 -0800

    Add error handling to tsukuru.bat for better debugging output

--- tsukuru.bat
+++ tsukuru.bat
@@ -5,8 +5,9 @@
                 :loop
                   if "%1" == "" goto :allprocessed
                   set files=%1 %2 %3 %4 %5 %6 %7 %8 %9
-                  java -jar %jarpath% %pattern% %files%
+                  java -jar %jarpath% %pattern% %files% 2>&1
+                  set exitcode=%ERRORLEVEL%
                   for %%i in (0 1 2 3 4 5 6 7 8) do shift
-                goto :loop
+                goto loop
 
                 :allprocessed
