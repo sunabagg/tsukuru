2c205c48bf4b8d70640f32d47608c50ffee0ca48
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 15:22:56 -0800

    Enhance help message display in main function for usage clarity

--- clisrc/Main.hx
+++ clisrc/Main.hx
@@ -3,7 +3,7 @@ package;
 class Main {
     public static function main() {
         var args = Sys.args();
-        if (args.length < 1) {
+        if (args.length < 1 || args[0] == "--help" || args[0] == "-h") {
             Sys.println("Usage: snbmake <project.snbproj>");
             return;
         }
