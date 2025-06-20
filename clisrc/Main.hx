ed03dc085eea07e73326cb8f013c10a5fc820057
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 06:29:45 -0800

    Add Main class with main function for WizMake CLI

---
+++ clisrc/Main.hx
@@ -0,0 +1,8 @@
+package clisrc;
+
+class Main {
+    public static function main() {
+        Sys.println("Welcome to WizMake CLI!");
+        Sys.println("This is a placeholder for the WizMake CLI functionality.");
+    }
+}
