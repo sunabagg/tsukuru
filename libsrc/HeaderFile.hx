ebe1624e6e71be23c13805aeb6d7f430dd96a833
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 11:35:46 -0800

    Add HeaderFile typedef with properties for name, version, rootUrl, luabin, and type

---
+++ libsrc/HeaderFile.hx
@@ -0,0 +1,9 @@
+package;
+
+typedef HeaderFile = {
+    var name: String;
+    var version: String;
+    var rootUrl: String;
+    var luabin: String;
+    var type: String; // "executable" or "library"
+}
