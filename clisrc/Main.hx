a3f31ba5b94c8a0c4636e3e20527cb3a86c0adb6
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-20 16:14:56 -0800

    Improve help message condition in main function for better user guidance

--- clisrc/Main.hx
+++ clisrc/Main.hx
@@ -3,7 +3,7 @@ package;
 class Main {
     public static function main() {
         var args = Sys.args();
-        if (args.length < 1) {
+        if (args.length < 1 || args[0] == "-h" || args[0] == "--help") {
             Sys.println("Usage: snbmake <project.snbproj>");
             return;
         }
