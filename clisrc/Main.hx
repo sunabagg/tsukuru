94c79798752e2d6bdcaefbf63303f9062193f579
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-24 16:13:48 -0800

    Renamed project to lfxbuild

--- clisrc/Main.hx
+++ clisrc/Main.hx
@@ -4,18 +4,18 @@ class Main {
     public static function main() {
         var args = Sys.args();
         if (args.length < 1 || args[0] == "-h" || args[0] == "--help") {
-            Sys.println("Usage: snbmake <project.snbproj>");
+            Sys.println("Usage: lfxbuild <project.lfxproj>");
             return;
         }
 
-        var tsukuru = new Tsukuru();
+        var lfxbuild = new Lfxbuild();
 
         var arg1 = args[1];
         if (arg1 == "-O") {
             var arg2 = args[2];
-            tsukuru.zipOutputPath = arg2;
+            lfxbuild.zipOutputPath = arg2;
         }
         
-        tsukuru.build(args[0]);
+        lfxbuild.build(args[0]);
     }
 }
